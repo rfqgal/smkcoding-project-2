eb198eeef36e5888cec67509e976632718e743b0
Rifky Galuh <56381009+rfqgal@users.noreply.github.com>
2020-05-10 01:57:10 +0700

    SMK Coding Challenge 2
    Membuat ViewPager2 dan TabLayout

--- app/src/main/java/com/example/smkcodingproject2/MainActivity.kt
+++ app/src/main/java/com/example/smkcodingproject2/MainActivity.kt
@@ -2,11 +2,25 @@ package com.example.smkcodingproject2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.res.ResourcesCompat
+import com.google.android.material.tabs.TabLayoutMediator
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val menuText = arrayOf("Friends", "GitHub", "Profile")
+    private val menuIcon = arrayOf(R.drawable.ic_home, R.drawable.ic_add, R.drawable.ic_profile)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val adapter = ViewPagerAdapter(this)
+        view_pager.setAdapter(adapter)
+        TabLayoutMediator(tab_layout, view_pager,
+            TabLayoutMediator.TabConfigurationStrategy { tab, position ->
+                tab.text = menuText[position]
+                tab.icon = ResourcesCompat.getDrawable(resources, menuIcon[position], null)
+            }).attach()
     }
 }
