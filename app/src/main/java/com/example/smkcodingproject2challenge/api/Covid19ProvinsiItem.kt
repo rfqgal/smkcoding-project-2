0b67923748510e08f9e283cfe43684f71fd792c2
Rifky Galuh <56381009+rfqgal@users.noreply.github.com>
2020-05-17 10:58:33 +0700

    SMK Coding Challenge 2
    Membuat aplikasi tentang Kesehatan (COVID-19) menggunakan Public APIs

--- app/src/main/java/com/example/smkcodingproject2challenge/api/Covid19ProvinsiItem.kt
+++ app/src/main/java/com/example/smkcodingproject2challenge/api/Covid19ProvinsiItem.kt
@@ -4,16 +4,6 @@ package com.example.smkcodingproject2challenge.api
 import com.google.gson.annotations.SerializedName
 
 data class Covid19ProvinsiItem(
-    @SerializedName("FID")
-    val fID: Int,
-    @SerializedName("Kasus_Meni")
-    val kasusMeninggal: Int,
-    @SerializedName("Kasus_Posi")
-    val kasusPositif: Int,
-    @SerializedName("Kasus_Semb")
-    val kasusSembuh: Int,
-    @SerializedName("Kode_Provi")
-    val kodeProvinsi: Int,
-    @SerializedName("Provinsi")
-    val provinsi: String
+    @SerializedName("attributes")
+    val attributes: Covid19ProvinsiAttributes
 )
